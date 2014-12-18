afca0baa460ebace401ecb1c539b47f6f4ff7e29
Isaac Sanders <isaac@isaacbfsanders.com>
2014-12-17 20:50:07 -0500

    More work on the initial information portion of the site

---
+++ app/controllers/learn_more_controller.rb
@@ -0,0 +1,5 @@
+class LearnMoreController < ApplicationController
+  def associate_membership
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  namespace :learn_more do
+    get :associate_membership
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
